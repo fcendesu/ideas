eaf249183c96136bdcff8de8df480411e5e60909
fcendesu <furkan.ckmk@outlook.com>
2025-01-28 21:46:24 +0200

    Add Idea model, migration, and update DashboardController to handle ideas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,19 @@
 //php artisan make:controller DashboardController
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view("dashboard");
+        $idea = new Idea([
+            'content' => 'This is a test idea',
+            'likes' => 0,
+        ]);
+        $idea->save();
+
+        return view('dashboard', ['ideas' => Idea::orderBy('created_at', 'desc')->get()]);
     }
 }
