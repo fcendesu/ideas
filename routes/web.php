4bc5c663577cc48918d95725c338f40afb1c4b34
fcendesu <furkan.ckmk@outlook.com>
2025-01-28 16:05:06 +0200

    Remove profile view and update dashboard view; add terms of service page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/terms', function () {
+    return view('terms');
+});
 
 
 Route::get('/profile', [ProfileController::class, 'index']);
