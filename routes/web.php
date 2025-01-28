883a4b2c7d05f27a7cc11f39afa9f0ed93ba02ca
fcendesu <furkan.ckmk@outlook.com>
2025-01-28 19:01:28 +0200

    Add terms of service page and refactor layout; remove redundant HTML from terms view

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index']);
+
 Route::get('/terms', function () {
     return view('terms');
 });
 
 
-Route::get('/profile', [ProfileController::class, 'index']);
+
